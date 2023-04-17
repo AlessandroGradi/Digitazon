a67013a9b39769248cac6c66339d5626f365d517
AlessandroGradi <alessandro.gradi@yahoo.it>
2023-04-17 20:41:34 +0200

    esercizi2

---
+++ esercitazioni/esercizi 17apr23/exA1.js
@@ -0,0 +1,9 @@
+// Data una stringa, ad esempio PAYPALISHIRING, 
+// scrivere una funzione che la ritorna a "ZigZag":
+
+//  P A H N
+//  APLSIIG
+//  Y I R
+
+// La funzione accetta in ingresso la stringa e un numero di righe, 
+// in questo caso e' stata invocata cosi: converti("PAYPALISHIRING", 3)
---
+++ esercitazioni/esercizi 17apr23/exA2.js
@@ -0,0 +1,12 @@
+// Data una stringa che contiene solo parentesi tonde aperte e parentesi tonde chiuse, 
+// scrivere una funzione che ritorna true se tutte le parentesi sono bilanciate, false altrimenti
+// ((()))()
+// ()
+// ()()()
+// ()(()))
+// console.log(isValid("((()))"))
+// console.log(isValid("((())))")) // false
+// console.log(isValid("((()))(())"))
+// console.log(isValid("((()))()"))
+// console.log(isValid("(()(()))"))
+// console.log(isValid("(()")) // false
