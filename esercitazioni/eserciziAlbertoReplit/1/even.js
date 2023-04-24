07004ac4dc237da59067ff63a506078f6e98c9d8
AlessandroGradi <alessandro.gradi@yahoo.it>
2023-04-24 12:13:27 +0200

    2

--- esercitazioni/eserciziAlbertoReplit/1/even.js
+++ esercitazioni/eserciziAlbertoReplit/1/even.js
@@ -2,5 +2,23 @@
 // dato un array di numeri in ingresso
 // ritorni un array che contiene solo i numeri pari
 
-function even(arr) {
+//dichiaro un array n di numeri interi
+//dichiaro un array vuoto npari
+//scrivo una funzione even con in ingresso 
+//itero sugli elementi di arr
+//se il numero corrispondente all'indice diviso per due da resto 0
+    //allora aggiungo il numero all'array npari
+
+let n = [1, 4, 56, 34, 67, 193, 1220, 6, 7]
+
+function even(n) {
+    let npari = []
+    for (i=0; i<n.length; i++) {
+        if (n[i]%2==0) {
+            npari.push(n[i])
+        } 
+    }
+    return npari
 }
+
+console.log(even(n))
--- esercitazioni/eserciziAlbertoReplit/1/isDivisible.js
+++ esercitazioni/eserciziAlbertoReplit/1/isDivisible.js
@@ -2,5 +2,14 @@
 // dato un numero a e un numero b
 // ritorni true se a e' divisibile per b, false altrimenti
 
+let a = 10
+let b = 5
 function isDivisible(a, b) {
+    if (a%b==0) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
 }
+
+isDivisible(a, b)
